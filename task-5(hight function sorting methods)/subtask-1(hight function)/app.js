644a1640cd21b098672da9164eb8874704f8bfeb
yurch-html2 <yurch-html@alterpost.org>
2020-07-15 23:51:57 +0300

    task 5

--- task-5(hight function sorting methods)/subtask-1(hight function)/app.js
+++ task-5(hight function sorting methods)/subtask-1(hight function)/app.js
@@ -1,20 +1,58 @@
 console.log('↓-----1-----↓');
 
 function firstFunc(arr, func) {
-	return 'New value: ' + func(arr);
-}
-
-function handler1(arr) {
 	let str = '';
 
-	for(let i = 0; i < arr.length; i++) {
-		str = str + arr[i][0].toUpperCase() + arr[i].slice(1,arr[i].lenght);
-	}
+	arr.forEach((item) => {
+		str = str + func(item)
+	});
 
-	return str;
+	return 'New value: ' + str.trim();
 }
 
-console.log(firstFunc(['my', 'name', 'is', 'Trinity'], handler1))
+function handler1(item) {
+	return item[0].toUpperCase() + item.slice(1, item.lenght);
+}
+
+function handler2(item) {
+	return item + 0 + ' ';
+}
+
+function handler3(item) {
+	return item.name + ' is ' + item.age + ', ';
+}
+
+function handler4(item) {
+	return item.split('').reverse().join('') + ' ';
+}
+
+console.log(firstFunc(['my', 'name', 'is', 'Trinity'], handler1));
+console.log(firstFunc([10, 20, 30], handler2));
+console.log(firstFunc([{age: 45, name: 'Jhon'}, {age: 20, name: 'Aaron'}], handler3));
+console.log(firstFunc(['abc', '123'], handler4));
 
 console.log('↑-----1-----↑');
 
+console.log('↓-----2-----↓');
+
+function funcEvery(arr, func) {
+	let status = false;
+
+	for (let i = 0; i < arr.length; i++) {
+		if (func(arr[i])) {
+			status = true;
+		} else {
+			status = false;
+		}
+	}
+
+	return status;
+}
+
+function everyHandler(element) {
+	return element >= 10;
+}
+
+console.log(funcEvery(['1', '2', '3'], everyHandler));
+
+console.log('↑-----2-----↑');
--- task-5(hight function sorting methods)/subtask-2(sorting methods)/app.js
+++ task-5(hight function sorting methods)/subtask-2(sorting methods)/app.js
@@ -1,4 +1,96 @@
 console.log('↓-----1-----↓');
 
+function isEven(arr) {
+	let newArr = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		newArr.push({digit: arr[i], odd: !!(arr[i] % 2)})
+	}
+
+	return newArr;
+}
+
+console.log(isEven([1, 2, 3]))
+
 console.log('↑-----1-----↑');
 
+console.log('↓-----2-----↓');
+
+function isNull(arr) {
+	let res = false;
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === 0) {
+			res = false;
+			break;
+		} else {
+			res = true;
+		}
+	}
+
+	return res;
+}
+
+console.log(isNull([12, 4, 50, 1, 1, 18, 40]));
+
+console.log('↑-----2-----↑');
+
+console.log('↓-----3-----↓');
+
+function lengthStr(arr) {
+	let res = false;
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i].length > 3) {
+			res = true;
+			break;
+		} else {
+			res = false;
+		}
+	}
+
+	return res;
+}
+
+console.log(lengthStr(['yes', 'hello', 'no', 'easycode', 'what']));
+
+console.log('↑-----3-----↑');
+
+console.log('↓-----4-----↓');
+
+let words = [
+	{char:"a",index:12},
+	{char:"w",index:8},
+	{char:"Y",index:10},
+	{char:"p",index:3},
+	{char:"p",index:2},
+	{char:"N",index:6},
+	{char:" ",index:5},
+	{char:"y",index:4},
+	{char:"r",index:13},
+	{char:"H",index:0},
+	{char:"e",index:11},
+	{char:"a",index:1},
+	{char:" ",index:9},
+	{char:"!",index:14},
+	{char:"e",index:7}
+	];
+
+function charWord(arr) {
+	let newArr = [];
+	let str = '';
+
+	newArr = arr.sort(function(a, b) {
+		return a.index - b.index;
+	});
+
+	for (let i = 0; i < newArr.length; i++) {
+		str = str + newArr[i].char;
+	}
+
+	return str;
+}
+
+console.log(charWord(words));
+
+console.log('↑-----4-----↑');

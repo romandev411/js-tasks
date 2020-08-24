2bac5875f92aa5bc66975946c410c738328c9b6a
yurch-html2 <yurch-html@alterpost.org>
2020-08-24 20:37:38 +0300

    task 6

--- tasks/task-6(dom)/subtask-1/app.js
+++ tasks/task-6(dom)/subtask-1/app.js
@@ -1,4 +1,39 @@
-let string = 'some test string';
+console.log('↓-----1-----↓');
 
+let elem1 = document.head;
+console.log(elem1)
 
 console.log('↑-----1-----↑');
+
+console.log('↓-----2-----↓');
+
+let elem2 = document.body;
+console.log(elem2)
+
+console.log('↑-----2-----↑');
+
+
+console.log('↓-----3-----↓');
+
+for (let i = 0; i < elem2.children.length; i++) {
+	console.log(elem2.children[i]);
+}
+
+console.log('↑-----3-----↑');
+
+console.log('↓-----4-----↓');
+
+let elem3 = document.body.children[0];
+let elem4 = document.body.children;
+console.log(elem3);
+console.log(elem4);
+
+const arrElem = [];
+
+for (let i = 1; i < elem4.length -1; i++) {
+	arrElem.push(elem4[i])
+}
+
+console.log(arrElem);
+
+console.log('↑-----4-----↑');
--- tasks/task-6(dom)/subtask-2/app.js
+++ tasks/task-6(dom)/subtask-2/app.js
@@ -1,4 +1,58 @@
 console.log('↓-----1-----↓');
 
+function isParent(parent, child) {
+	let status = false;
+
+	for (let i = 0; i < 10; i++) {
+		child = child.parentElement;
+
+		if (!child) {
+			break;
+		}
+
+		if (child === parent) {
+			status = true;
+			break;
+		}
+	}
+
+	return status;
+}
+
+const func = isParent(document.body.children[0], document.querySelector('article'));
+console.log(func);
 
 console.log('↑-----1-----↑');
+
+console.log('↓-----2-----↓');
+
+const ignore = document.querySelector('ul')
+const elem = 'a';
+
+function searchTag(ignorPArent, elem) {
+	const arrTags = [];
+	const tags = document.querySelectorAll(elem)
+
+	for(let i = 0; i < tags.length; i++) {
+
+		if (!isParent(ignorPArent, tags[i])) {
+			arrTags.push(tags[i]);
+			console.log(tags[i])
+		}
+	}
+
+	return arrTags;
+};
+
+const func2 = searchTag(ignore, elem);
+console.log(func2);
+
+console.log('↑-----2-----↑');
+
+console.log('↓-----3-----↓');
+
+const ulItem = document.querySelector('ul');
+console.log(ulItem.previousElementSibling);
+console.log(ulItem.nextElementSibling);
+
+console.log('↑-----3-----↑');
